2f69120e1f4955e90735e481529cd7947796d187
Nick Schiffke <Nick.Schiffke@yahoo.com>
2022-06-16 09:39:57 +0200

    why does other router file not work? (copy into routers.js does work)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
-const routes = require("./javascript/modules/routes/routes");
+//const routes = require("./javascript/modules/routes/routes");
+const test_route = require("./javascript/modules/routes/test_route");
 
 const app = express();
 
@@ -11,7 +12,8 @@ app
     .set("view engine", "pug")
     .set("views", path.join(__dirname, "views"))
 
-    .use("/", routes)
+    //.use("/", routes)
+    .use("/", test_route)
   
     .use(
         "/css",
--- src/javascript/modules/routes/routes.js
+++ src/javascript/modules/routes/routes.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-let test_router = require("./test_route");
-
 router.get("/", (req, res) => {
     res.render("index", {
         //vars
     });
 });
-
-router.use("/test", test_router);
 
 router.get("/about", (req, res) => {
     res.render("about", {
