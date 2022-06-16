1ec4b866b28ef65ae74a055c614ae0084ccb720e
Nick Schiffke <Nick.Schiffke@yahoo.com>
2022-06-16 15:11:32 +0200

    wrote functions to compare passwords in tryout

--- src/javascript/tryout.js
+++ src/javascript/tryout.js
@@ -1,23 +1,68 @@
+const bcrypt = require("bcrypt");
 
-/*const exercises = require("./modules/table_classes/exercises");
-
-async function blub() {
-    let exercise = new exercises("sad", "afgag");
-    //xercise.write_table()
-    let stuff = await exercise.read_table();
-    console.log(stuff);
+function encryptPw(password, callback) {
+    bcrypt.genSalt(12, (err, salt) => {
+        if (err)
+            return callback(err);
+        
+        bcrypt.hash(password, salt, (err, hash) => {
+            return callback(err, hash);
+        });
+    });
 }
 
-blub();
-
-*/
-
-const p_e = require("./modules/table_classes/personalized_exercises");
-
-async function testInnerJoin() {
-    let pe_interface = new p_e();
-    let data = await pe_interface.innerJoinExercises();
-    console.log(data);
+async function encryptPw2(password) {
+    return new Promise((res, rej) => {
+        bcrypt.genSalt(12, (err, salt) => {
+            if (err) {
+                rej(err);
+            } else {
+                bcrypt.hash(password, salt, (err, hash) => {
+                    if (err) {
+                        rej(err);
+                    } else {
+                        res(hash);
+                    }
+                });
+            }
+        });
+    });
 }
 
-testInnerJoin();
+function comparePasswords(textPw, hashPw, callback) {
+    bcrypt.compare(textPw, hashPw, (err, pw_matches) => {
+        if (err) {
+            return callback(err);
+        }
+        return callback(err, pw_matches);
+    }) ;
+}
+
+async function comparePasswords2(textPw, hashPw) {
+    return new Promise((res, rej) => {
+        bcrypt.compare(textPw, hashPw, (err, pw_matches) => {
+            if (err) {
+                rej(err);
+            } else {
+                res(pw_matches);
+            }
+        });
+    });
+}
+
+async function evalPwCompare(pw, hashPw) {
+    let result = await comparePasswords2(pw, hashPw);
+    console.log(result);
+}
+
+async function testEncryption() {
+    console.log("expectation: first true then false");
+    
+    let enc_pw = await encryptPw2("123456");
+    evalPwCompare("123456", enc_pw);
+    
+    let enc_pw2 = await encryptPw2("123456");
+    evalPwCompare("1234567", enc_pw2);
+}
+
+testEncryption();
