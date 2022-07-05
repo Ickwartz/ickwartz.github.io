2cbdcf8530a3aaa7d8c98497305e38a236373f23
Nick Schiffke <Nick.Schiffke@yahoo.com>
2022-07-05 11:48:59 +0200

    changed way of bypassing authentication from in routes to in pug

--- src/javascript/modules/routes/routes.js
+++ src/javascript/modules/routes/routes.js
@@ -23,7 +23,7 @@ router
 
     .get("/training", (req, res) => {
         res.render("training", {
-            loggedin: true //req.session.loggedin ? true : false
+            loggedin: req.session.loggedin ? true : false
         });
     })
 
