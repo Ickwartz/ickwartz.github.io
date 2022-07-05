69e0296a5e67db9510ad922efdd01e4bb3dc7ce5
Nick Schiffke <Nick.Schiffke@yahoo.com>
2022-07-05 09:31:58 +0200

    split calendar functionality into js modules

---
+++ src/javascript/client-side/modules/backendCommunication.js
@@ -0,0 +1,27 @@
+const fakeSchedule = [
+    {
+        date: "2022-06-30",
+        name: "Workout",
+        description: "Workout im Juni"
+    },
+    {
+        date: "2022-07-04",
+        name: "Geburtstag",
+        description: "Ein Geburtstag"
+    },
+    {
+        date: "2022-07-10",
+        name: "Workout",
+        description: "Workout im Juli"
+    }
+];
+
+
+
+// -------------- TODO ---------------
+function getAppointments() {
+    return fakeSchedule;
+}
+
+
+export { getAppointments };
