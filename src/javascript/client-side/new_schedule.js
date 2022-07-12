f6fc51bcd08300610aa95e48ac230a33b4dabe61
Nick Schiffke <Nick.Schiffke@yahoo.com>
2022-07-12 17:20:09 +0200

    added focus on next input on inputs when pressing enter and createNewRow when pressing enter on last textarea

--- src/javascript/client-side/new_schedule.js
+++ src/javascript/client-side/new_schedule.js
@@ -1,7 +1,34 @@
 import {NewScheduleTableHandler} from "./modules/newScheduleTableHandler.js";
+let tableHandler =  new NewScheduleTableHandler();
+
+function applyKeyListeners() {
+	let inputs = document.querySelectorAll("input, textarea");
+	let nodes = Array.prototype.slice.call(inputs);
+	console.log(inputs);
+	for (let input of inputs) {
+		let index = nodes.indexOf(input);
+		input.addEventListener("keypress", (e) => {
+			if (e.key === "Enter") {
+				inputs[index+1] ? inputs[index+1].focus() : inputs[index].focus();
+			}
+		});
+		
+	}
+	let textareas = document.getElementsByTagName("textarea");
+	let textnodes = Array.prototype.slice.call(textareas);
+	for (let textarea of textareas) {
+		let index = textnodes.indexOf(textarea);
+		textarea.addEventListener("keypress", (e) => {
+			if (e.key === "Enter") {
+				if (!textareas[index+1]) {
+					tableHandler.createRow();
+				}
+			}
+		});
+	}
+}
 
 window.onload = () => {
-	let tableHandler =  new NewScheduleTableHandler();
 
 	document.getElementById("addRowButton").addEventListener("click",() => {
 		tableHandler.createRow();
@@ -11,4 +38,10 @@ window.onload = () => {
         tableHandler.saveTableData();
     }); 
 
+	applyKeyListeners();
+	let tbody = document.getElementById("input_body");
+	const observer = new MutationObserver(async () => {
+		applyKeyListeners();
+    });
+    observer.observe(tbody, {childList: true});
 };
