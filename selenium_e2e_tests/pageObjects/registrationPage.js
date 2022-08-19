8e890df1fb9d8f3b898a463513afcc047a025e61
Nick Schiffke <Nick.Schiffke@yahoo.com>
2022-08-19 09:26:46 +0200

    registration test working

--- selenium_e2e_tests/pageObjects/registrationPage.js
+++ selenium_e2e_tests/pageObjects/registrationPage.js
@@ -1,4 +1,4 @@
-const { By } = require("selenium-webdriver");
+const { By, until } = require("selenium-webdriver");
 const Page = require("./page");
 
 class RegistrationPage extends Page {
@@ -13,48 +13,43 @@ class RegistrationPage extends Page {
     async enterFirstName(name) {
         let input = await this.driver.findElement(By.name("first_name"));
         await input.sendKeys(name);
-        return this;
     }
 
     async enterSurname(name) {
         let input = await this.driver.findElement(By.name("surname"));
         await input.sendKeys(name);
-        return this;
     }
 
     async enterEmail(mail) {
         let input = await this.driver.findElement(By.name("email"));
         await input.sendKeys(mail);
-        return this;
     }
 
     async enterPassword(pw) {
         let input = await this.driver.findElement(By.name("password"));
         await input.sendKeys(pw);
-        return this;
     }
 
     async enterPasswordConfirm(pw) {
         let input = await this.driver.findElement(By.name("password_confirm"));
         await input.sendKeys(pw);
-        return this;
     }
 
     async submit() {
         let submitButton = await this.driver.findElement(By.css('button[type="submit"]'));
         await await submitButton.click();
-        return this;
+        let re = new RegExp(`^(?!(${this.baseUrl+"register"})$).*$`, "g");  // alles das nicht baseURL/register ist
+        await this.driver.wait(until.urlMatches(re), 5000, "Timed out after submit", 500);
     }
 
     async alreadyRegistered() {
-        let infoMessage = await this.driver.findElements(By.css('h4[text="Es ist schon ein Nutzer mit dieser Email registriert."]'));
+        let infoMessage = await this.driver.findElements(By.xpath('//h4[text()="Es ist schon ein Nutzer mit dieser Email registriert."]'));
         console.log(infoMessage);
         return infoMessage.length > 0 ? true : false;
     }
 
     async notPreregistered() {
-        let infoMessage = await this.driver.findElements(By.css('h4[text="Bitte melden Sie sich bei mir an, bevor Sie sich auf dieser Seite registrieren."]'));
-        console.log(infoMessage);
+        let infoMessage = await this.driver.findElements(By.xpath('//h4[text()="Bitte melden Sie sich bei mir an, bevor Sie sich auf dieser Seite registrieren."]'));
         return infoMessage.length > 0 ? true : false;
     }
 }
