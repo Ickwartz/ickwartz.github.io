c33559948bb7590e280a626e5ad0851514845dc6
Nick Schiffke <Nick.Schiffke@yahoo.com>
2022-08-19 12:45:20 +0200

    hashing tests completed

--- src/javascript/modules/helpers/__tests__/hashing.test.js
+++ src/javascript/modules/helpers/__tests__/hashing.test.js
@@ -7,7 +7,7 @@ describe("Hashing Test", () => {
         bcrypt.resetParams();
     });
 
-    test("hashPw should reject with error on bcrypt.genSalt Error", async () => {
+    test("hashPw should reject with error on bcrypt.genSalt Error", () => {
         bcrypt.genSaltError = true;
         let result = hashing.hashPw("password");
         expect(result).rejects.toThrow("salt Error");
@@ -19,9 +19,26 @@ describe("Hashing Test", () => {
         expect(result).rejects.toThrow("hash Error");
     });
 
+    test("hashPw resolves to bcrypt.hash result", () => {
+        let result = hashing.hashPw("password");
+        expect(result).resolves.toBe("hashedPw");
+    });
+
     test("comparePasswords should reject with error on bcrypt.compare Error", () => {
         bcrypt.compareError = true;
         let result = hashing.comparePasswords("password", "hashedPassword");
         expect(result).rejects.toThrow("compare Error");
+    });
+
+    test("comparePasswords resolves to true when bcrypt.compare returns true", () => {
+        bcrypt.compareSuccess = true;
+        let result = hashing.comparePasswords("password", "hashedPassword");
+        expect(result).resolves.toBe(true);
+    });
+
+    test("comparePasswords resolves to false when bcrypt.compare returns false", () => {
+        bcrypt.compareSuccess = false;
+        let result = hashing.comparePasswords("password", "hashedPassword");
+        expect(result).resolves.toBe(false);
     });
 });
