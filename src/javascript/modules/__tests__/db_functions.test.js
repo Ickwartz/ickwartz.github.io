d520fe0b8c84a614723bc68a7796118ba9a6de75
Nick Schiffke <Nick.Schiffke@yahoo.com>
2022-08-30 11:56:00 +0200

    user_accounts test finished

--- src/javascript/modules/__tests__/db_functions.test.js
+++ src/javascript/modules/__tests__/db_functions.test.js
@@ -8,38 +8,41 @@ describe("Db_Functions", () => {
         db_instance.resetParams();
     });
     let db_functions = new Db_Functions();
-
-    test("queryAll throws Error when db.prepare returns error", async () => {
-        db_instance.prepareError = true;
-        let params = {key: "value"};
-        await expect(db_functions.queryAll("sql", params)).rejects.toThrow(/prepare Error/);
+    describe("queryAll", () => {
+        test("throws Error when db.prepare returns error", async () => {
+            db_instance.prepareError = true;
+            let params = {key: "value"};
+            await expect(db_functions.queryAll("sql", params)).rejects.toThrow(/prepare Error/);
+        });
+    
+        test("throws Error when query.all returns error", async () => {
+            db_instance.queryAllError = true;
+            let params = {key: "value"};
+            await expect(db_functions.queryAll("sql", params)).rejects.toThrow(/all Error/);
+        });
+    
+        test("resolves when no errors are returned", async () => {
+            let params = {key: "value"};
+            await expect(db_functions.queryAll("sql", params)).resolves.toBeTruthy();
+        });
     });
-
-    test("queryAll throws Error when query.all returns error", async () => {
-        db_instance.queryAllError = true;
-        let params = {key: "value"};
-        await expect(db_functions.queryAll("sql", params)).rejects.toThrow(/all Error/);
-    });
-
-    test("queryAll resolves when no errors are returned", async () => {
-        let params = {key: "value"};
-        await expect(db_functions.queryAll("sql", params)).resolves.toBeTruthy();
-    });
-
-    test("runQuery throws Error when db.prepare returns error", async () => {
-        db_instance.prepareError = true;
-        let params = {key: "value"};
-        await expect(db_functions.runQuery("sql", params)).rejects.toThrow(/prepare Error/);
-    });
-
-    test("runQuery throws Error when query.run returns error", async () => {
-        db_instance.queryRunError = true;
-        let params = {key: "value"};
-        await expect(db_functions.runQuery("sql", params)).rejects.toThrow(/run Error/);
-    });
-
-    test("runQuery resolves when no errors are returned", async () => {
-        let params = {key: "value"};
-        await expect(db_functions.runQuery("sql", params)).resolves.toBeTruthy();
+    
+    describe("runQuery", () => {
+        test("throws Error when db.prepare returns error", async () => {
+            db_instance.prepareError = true;
+            let params = {key: "value"};
+            await expect(db_functions.runQuery("sql", params)).rejects.toThrow(/prepare Error/);
+        });
+    
+        test("throws Error when query.run returns error", async () => {
+            db_instance.queryRunError = true;
+            let params = {key: "value"};
+            await expect(db_functions.runQuery("sql", params)).rejects.toThrow(/run Error/);
+        });
+    
+        test("resolves when no errors are returned", async () => {
+            let params = {key: "value"};
+            await expect(db_functions.runQuery("sql", params)).resolves.toBeTruthy();
+        });
     });
 });
--- src/javascript/modules/table_classes/__tests__/user_accounts.test.js
+++ src/javascript/modules/table_classes/__tests__/user_accounts.test.js
@@ -1,17 +1,66 @@
 const User_Account = require("../user_accounts");
+const db_functions = require("../../db_functions");
+const hashing = require("../../helpers/hashing");
 
 jest.mock("../../db_functions");
+// jest.mock("../../helpers/hashing");
+
+hashing.comparePasswords = jest.fn();
 
 describe("user_accounts", () => {
-    afterEach(() => jest.clearAllMocks());       // reset mocks after each test to prevent errors when adding more tests
+    afterEach(() => jest.clearAllMocks());
 
     let testUser = new User_Account();
-    test("Resolve to true when queryAll returns data", async () => {
-        expect(await testUser.verifyUser()).toBe(true);
-    });
-    
-    test("Resolve to false when queryAll returns no data", async () => {
-        expect(await testUser.verifyUser()).toBe(false);
+    describe("verifyUser", () => {
+        test("return false when queryAll returns empty array", async () => {
+            db_functions.prototype.queryAll.mockReturnValueOnce([]);
+            expect(await testUser.verifyUser()).toBe(false);
+        });
+        test("return false when queryAll returns smth but comparePasswords returns false", async () => {
+            db_functions.prototype.queryAll.mockReturnValueOnce(["foo"]);
+            hashing.comparePasswords.mockReturnValueOnce(false);
+            expect(await testUser.verifyUser()).toBe(false);
+        });
+        test("return true when queryAll returns smth and comparePasswords returns true", async () => {
+            db_functions.prototype.queryAll.mockReturnValueOnce(["foo"]);
+            hashing.comparePasswords.mockReturnValueOnce(true);
+            expect(await testUser.verifyUser()).toBe(true);
+        });
     });
 
+    describe("isAdmin", () => {
+        test("return false when queryAll returns empty array", async () => {
+            db_functions.prototype.queryAll.mockReturnValueOnce([]);
+            expect(await testUser.isAdmin()).toBe(false);
+        });
+        test("return false when queryAll returns array with at least one value", async () => {
+            db_functions.prototype.queryAll.mockReturnValueOnce(["foo"]);
+            expect(await testUser.isAdmin()).toBe(true);
+        });
+    });
+
+    describe("isRegistered", () => {
+        test("return false when queryAll returns empty array", async () => {
+            db_functions.prototype.queryAll.mockReturnValueOnce([]);
+            expect(await testUser.isRegistered()).toBe(false);
+        });
+        test("return false when queryAll returns array with at least one value", async () => {
+            db_functions.prototype.queryAll.mockReturnValueOnce(["foo"]);
+            expect(await testUser.isRegistered()).toBe(true);
+        });
+    });
+
+    describe("safeData", () => {
+        test("runs runQuery", async () => {
+            testUser.safeData();
+            await expect(db_functions.prototype.runQuery).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    describe("readData", () => {
+        test("runs queryAll", async () => {
+            testUser.readData();
+            await expect(db_functions.prototype.queryAll).toHaveBeenCalledTimes(1);
+        });
+    });
 });
